d1d353caba8f5a8dd61f56062c1c9fa209a530f2
PLOTO <abdallhdawi3@gmail.com>
2026-08-22 17:57:37 +0300

    Completed the first five Foundation problems

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -9,7 +9,9 @@
 
 // Add your code right below, good luck!
 
+let firstName = "Carlos", lastName = "Stevenson", thisYear = 1965, birthYear = 1947;
 
+let greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
 
 
 
